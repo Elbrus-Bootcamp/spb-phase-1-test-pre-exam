a3fd0595f01ce68595ffc18e7ad5faf66346a4e4
Max Kosheutov <maxkosh1994@cmail.com>
2024-06-26 14:08:28 +0300

    issue

--- CarSaver.js
+++ CarSaver.js
@@ -2,14 +2,19 @@ const fs = require('fs').promises;
 const { Car } = require('./db/models');
 
 class CarSaver {
+  //* Метод для чтения файла
   static async loadDataFromFile(filePath) {}
 
+  //* Метод преобразования данных из файла в массив объектов без лишних данных
   static reorganizeData(data) {}
 
+  //* Метод поиска авто со скоростью выше 250км/ч
   static searchFastCars(reorganizedData) {}
 
+  //* Метод записи массива объектов авто в базу данных
   static async writeDataToDB(cars) {}
 
+  //* Метод вывода новых данных из базы (УЖЕ РЕАЛИЗОВАН)
   static async getCarsDataFromDB() {
     try {
       const allCars = await Car.findAll({ raw: true });
